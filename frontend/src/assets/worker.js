11d5bab7f546564e26968db124faef9f8035c94d
Rémi Connesson <remiconnesson@gmail.com>
2022-08-22 18:37:53 +0200

    Minimal example of using a worker with vue

--- frontend/src/assets/worker.js
+++ frontend/src/assets/worker.js
@@ -1,7 +1,11 @@
 self.postMessage("Hello from the worker");
 
+import json from "@/assets/input.json";
+
+const prefix = json.prefix;
+
 console.log("Worker is executed");
 
 self.onmessage = (event) => {
-  self.postMessage(`I am the worker and I received: ${event.data}`);
+  self.postMessage(`${prefix} : I am the worker and I received: ${event.data}`);
 };
