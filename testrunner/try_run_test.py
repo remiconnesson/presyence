ab67c0a14342cd09ff41989fefaf58d92ea3580b
Rémi Connesson <remiconnesson@gmail.com>
2022-08-23 10:20:30 +0200

    Get dataframes as csv string (for the report)

--- testrunner/try_run_test.py
+++ testrunner/try_run_test.py
@@ -6,6 +6,10 @@ from demo import tests
 def inspect_test(test: SimpleTest):
     print(inspect.getsource(test.function))
 
+def get_df_as_csv(test: SimpleTest):
+    print(test.test_input.to_csv(index=False))
+    print(test.test_output.to_csv(index=False))
+
 
 def run_tests(tests: list[TestGroup | SimpleTest]):
     for test in tests:
@@ -17,6 +21,7 @@ def run_tests(tests: list[TestGroup | SimpleTest]):
             elif isinstance(test, SimpleExceptionTest):
                 print("SET", test)
             inspect_test(test)
+            get_df_as_csv(test)
 
 
 run_tests(tests)
