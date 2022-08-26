dbd0aa31d77f47a8b398cf4dbb7015fed54e7719
Rémi Connesson <remiconnesson@gmail.com>
2022-08-26 17:42:11 +0200

    Exit if no test files are found

--- presyence/presyence/main.py
+++ presyence/presyence/main.py
@@ -1,5 +1,5 @@
 import typer
-from .testrunner.discovery import discover_test_files
+from .testrunner.discovery import discover_test_files, extract_tests_from_file
 
 app = typer.Typer()
 
@@ -9,10 +9,15 @@ def main():
     print("Looking for tests")
     test_files = discover_test_files()
     print(test_files)
-    if "No test file found":
-        typer.Abort()
-    if "No tests found in file":
-        typer.Abort()
+    if not test_files:
+        print("No test file found. Test file names must end with '.spec.sy.py'")
+        exit()
+    tests = []
+    for test_file in test_files:
+        _tests = extract_tests_from_file(test_file)
+        if not _tests:
+            print(f"No tests found in file {test_file.absolute()}")
+            exit()
     print("Running tests")
     print("Creating the test report")
     print("Serving the test report on http://localhost:5000/")
