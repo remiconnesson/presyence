5e77a2f53897e945aa544b99a4337b16bbe06687
Rémi Connesson <remiconnesson@gmail.com>
2022-08-28 19:39:47 +0200

    Script to clone frontend build into package

---
+++ clone_frontend_build_into_package.sh
@@ -0,0 +1,4 @@
+rm -rf presyence/presyence/reporter/www/*
+cp -r frontend/dist/* presyence/presyence/reporter/www
+touch presyence/presyence/reporter/www/__init__.py
+touch presyence/presyence/reporter/www/assets/__init__.py
