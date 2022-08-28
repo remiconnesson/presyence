7fd69216375369dd7ad27a707e3438f059656f31
Rémi Connesson <remiconnesson@gmail.com>
2022-08-28 14:31:49 +0200

    define the API for simple tests

--- presyence/presyence/simpletests.py
+++ presyence/presyence/simpletests.py
@@ -1,1 +1,23 @@
+from dataclasses import dataclass
+from collections.abc import Callable
+import pandas as pd
 
+
+@dataclass
+class SimpleTest:
+    title: str
+    function: Callable
+    input: pd.DataFrame | pd.Series
+    output: pd.DataFrame | pd.Series
+
+    def __radd__(self, tests: list):
+        if isinstance(tests, list):
+            tests.append(self)
+            return tests
+        else:
+            raise TypeError(
+                (
+                    "presyence SimpleTest can only be added to a list, "
+                    f"found:, {type(tests)}"
+                )
+            )
