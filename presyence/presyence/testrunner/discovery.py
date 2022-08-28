e344e4f546fee0b8524389fdaa18c0ed18040d94
Rémi Connesson <remiconnesson@gmail.com>
2022-08-28 14:30:29 +0200

    handle rel. import in dyn. loaded module

--- presyence/presyence/testrunner/discovery.py
+++ presyence/presyence/testrunner/discovery.py
@@ -2,6 +2,8 @@
 """
 from pathlib import Path
 import importlib.machinery
+import importlib.util
+import sys
 import types
 
 
@@ -13,10 +15,20 @@ def discover_test_files(pattern="**/*.spec.sy.py", directory="."):
     )  # return a list of files instead of an iterator for convenience
 
 
-def extract_tests_from_file(file, module_name="testmodule"):
-    loader = importlib.machinery.SourceFileLoader(module_name, str(file))
-    testmodule = types.ModuleType(module_name)
-    loader.exec_module(testmodule)
+def extract_tests_from_file(file):
+    # get the true absolute name of the file
+    file = file.resolve()
+    parent, name = file.parent, file.name
+    # add parent to path to help with relative imports 
+    if parent not in sys.path:
+        sys.path.append(str(parent))
+    # in the next lines we will import the file
+    module_name = name
+    spec = importlib.util.spec_from_file_location(module_name, file)
+    # test_module is the .py containing the tests to run 
+    test_module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = test_module
+    spec.loader.exec_module(test_module)
     # check that a lists named TESTS is present
-    tests = testmodule.__dict__.get("TESTS", [])
+    tests = test_module.__dict__.get("TESTS", [])
     return tests
