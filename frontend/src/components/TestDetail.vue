35d2b0d113f4c650009036986226b3165ffaa3d6
Rémi Connesson <remiconnesson@gmail.com>
2022-08-29 22:02:58 +0200

    Handle hard refresh on the TestDetail component

--- frontend/src/components/TestDetail.vue
+++ frontend/src/components/TestDetail.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
 import { useRoute } from "vue-router";
 import { useTestsReportsStore } from "@/stores/testsReports";
+import { computed } from "vue";
+import type { TestReport } from "@/testReport";
 
 const route = useRoute();
 const testsReportsStore = useTestsReportsStore();
 
-const test = testsReportsStore.testsReports[route.params.index];
+const test = computed<TestReport>(() => {
+  const index = parseInt(route.params.index as string);
+  return testsReportsStore.testsReports[index];
+});
 </script>
 
 <template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+const store = createPinia();
+
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(store);
 app.use(router);
 
 app.mount("#app");
