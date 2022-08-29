3c5cd35778188d0564059ca33d084f3a8c516832
Rémi Connesson <remiconnesson@gmail.com>
2022-08-29 22:19:28 +0200

    Format the test report with pre blocks

--- frontend/src/components/TestDetail.vue
+++ frontend/src/components/TestDetail.vue
@@ -3,6 +3,8 @@ import { useRoute } from "vue-router";
 import { useTestsReportsStore } from "@/stores/testsReports";
 import { computed } from "vue";
 import type { TestReport } from "@/testReport";
+import CodeBlock from "@/components/CodeBlock.vue";
+import DataFrameBlock from "@/components/DataFrameBlock.vue";
 
 const route = useRoute();
 const testsReportsStore = useTestsReportsStore();
@@ -14,5 +16,27 @@ const test = computed<TestReport>(() => {
 </script>
 
 <template>
-  {{ test }}
+  <pre>
+  {{ test.test.title }}
+  {{ test.result.status }}
+  </pre>
+  <CodeBlock>
+    {{ test.test.function }}
+  </CodeBlock>
+  <DataFrameBlock>
+    {{ test.test.input }}
+  </DataFrameBlock>
+  <DataFrameBlock>
+    {{ test.test.expected_output }}
+  </DataFrameBlock>
+  <DataFrameBlock v-if="test.result.status === 'WrongResult'">
+    {{ test.result.testrun_output }}
+  </DataFrameBlock>
+  <CodeBlock v-if="test.result.status === 'WrongResult'">
+    {{ test.result.assertion_error_message }}
+  </CodeBlock>
+  <CodeBlock v-if="test.result.status === 'Crash'">
+    TRACEBACK
+    {{ test.result.traceback }}
+  </CodeBlock>
 </template>
