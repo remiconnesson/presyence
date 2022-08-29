5aaa32bf400ef7c3c0c5fcbc8e97a85e59b37e12
Rémi Connesson <remiconnesson@gmail.com>
2022-08-29 22:32:48 +0200

    Add a back button to detail view

--- frontend/src/views/TestDetailView.vue
+++ frontend/src/views/TestDetailView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import TestDetail from "@/components/TestDetail.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
+  <RouterLink to="/">Back</RouterLink>
   <TestDetail></TestDetail>
 </template>
