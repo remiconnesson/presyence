b0f214f1e04ab31aeebfa7bd6e2d42d8f7173e1a
Rémi Connesson <remiconnesson@gmail.com>
2022-08-31 23:42:35 +0200

    Install BULMA CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@ import { RouterView } from "vue-router";
 </template>
 
 <style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
 @import url("https://fonts.googleapis.com/css2?family=Karla&display=swap");
 @import url(https://fonts.googleapis.com/css?family=Inconsolata:400,700|Open+Sans:400,800);
 @import "highlight.js/styles/mono-blue.css";
