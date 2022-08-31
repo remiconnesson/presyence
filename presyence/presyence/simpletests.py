81ad983082b5e2cbcc206b164a2404942c46468a
Rémi Connesson <remiconnesson@gmail.com>
2022-08-31 18:02:27 +0200

    SimpleTest now enforces the correct types

--- presyence/presyence/simpletests.py
+++ presyence/presyence/simpletests.py
@@ -20,6 +20,16 @@ class SimpleTest:
     input: pd.DataFrame | pd.Series
     expected_output: pd.DataFrame | pd.Series
 
+    def __post_init__(self):
+        """
+        Enforces correct typing.
+        """
+        for (name, field_type) in self.__annotations__.items():
+            if not isinstance(self.__dict__[name], field_type):
+                current_type = type(self.__dict__[name])
+                raise TypeError(f"The field `{name}` was assigned by `{current_type}` instead of `{field_type}`")
+
+
     def export(self):
         return SimpleTestExport(
             title=self.title,
