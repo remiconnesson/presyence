65a94878c789c3479522475f0a83d66f74b7c488
Rémi Connesson <remiconnesson@gmail.com>
2022-08-31 21:53:24 +0200

    Add comments to inject_payload

--- presyence/presyence/reporter/inject_json_into_js.py
+++ presyence/presyence/reporter/inject_json_into_js.py
@@ -1,23 +1,29 @@
 import json
 
 
-def inject_payload(content, payload):
-    # insert a place holder easier to replace
-    begin_payload, end_payload = "beginPayload:!0,", ",endPayload:!0"
+def inject_payload(
+    content,
+    payload,
+    begin_payload="beginPayload:!0,",
+    end_payload=",endPayload:!0"
+):
+    """inject into `content` a json dumps of the `payload` between `begin_payload` and `end_payload`"""
+    # locate the start and end of the placeholder
     start_placeholder = content.index(begin_payload) + len(begin_payload)
     end_placeholder = content.index(end_payload)
+    # insert a place holder easier to replace instead of the current placeholder
     content = list(content)
     new_placeholder = "REPLACEMEPLEASE"
     content[start_placeholder:end_placeholder] = list(new_placeholder)
     content = "".join(content)
-
+    # transform the payload into json to be inserted in the middle of another json string
     new_payload = json.dumps(
-        payload, separators=(",", ":")
-    )  # separators with out whitespace
+        payload, separators=(",", ":")  # separators with out whitespace to minify it
+    )
     trim_first_and_last_character = lambda s: s[1:-1]  # remove `{` and `}`
     new_payload = trim_first_and_last_character(new_payload)
 
-    # inject the string in place of the placeholder
+    # inject the stringfied payload in place of the placeholder
     content = content.replace(new_placeholder, new_payload)
 
     return content
