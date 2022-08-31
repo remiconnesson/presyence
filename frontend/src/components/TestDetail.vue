989a4512ce76802e8ded70ab657cf2029f2eca69
Rémi Connesson <remiconnesson@gmail.com>
2022-09-01 00:10:29 +0200

    Improving test detail view

--- frontend/src/components/TestDetail.vue
+++ frontend/src/components/TestDetail.vue
@@ -16,43 +16,46 @@ const test = computed<TestReport>(() => {
 </script>
 
 <template>
-  <h1>{{ test.result.status }} -- {{ test.test.title }}</h1>
+  <h1 class="title">
+    <span class="tag is-medium">{{ test.result.status }}</span>
+    {{ test.test.title }}
+  </h1>
   <div
-    class="detail-card"
+    class="detail-card block"
     v-if="test.result.status === 'WrongResult'"
     data-testid="test-detail-pandas-error"
   >
-    <h2>Pandas Error Message</h2>
+    <h2 class="subtitle">Pandas Error Message</h2>
     <CodeBlock :code="test.result.assertion_error_message" />
   </div>
-  <div class="detail-card" data-testid="test-detail-expected-output">
-    <h2>The Expected Result</h2>
+  <div class="detail-card block" data-testid="test-detail-expected-output">
+    <h2 class="subtitle">The Expected Result</h2>
     <DataFrameBlock :csv="test.test.expected_output" />
   </div>
   <div
-    class="detail-card"
+    class="detail-card block"
     v-if="test.result.status === 'WrongResult'"
     data-testid="test-detail-testrun-output"
   >
-    <h2>The Observed Result</h2>
+    <h2 class="subtitle">The Observed Result</h2>
     <DataFrameBlock :csv="test.result.testrun_output" />
   </div>
 
   <div
-    class="detail-card"
+    class="detail-card block"
     v-if="test.result.status === 'Crash'"
     data-testid="test-detail-traceback"
   >
-    <h2>Crash Traceback</h2>
+    <h2 class="subtitle">Crash Traceback</h2>
     <CodeBlock :code="test.result.traceback" />
   </div>
-  <div class="detail-card" data-testid="test-detail-function">
-    <h2>Tested Function</h2>
+  <div class="detail-card block" data-testid="test-detail-function">
+    <h2 class="subtitle">Tested Function</h2>
     <CodeBlock :code="test.test.function" />
   </div>
 
-  <div class="detail-card" data-testid="test-detail-input">
-    <h2>The Test Input</h2>
+  <div class="detail-card block" data-testid="test-detail-input">
+    <h2 class="subtitle">The Test Input</h2>
     <DataFrameBlock :csv="test.test.input" />
   </div>
 </template>
