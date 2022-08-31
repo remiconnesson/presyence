b7f2354bf1edf6ca999b9e092e7471c454b85cac
Rémi Connesson <remiconnesson@gmail.com>
2022-08-31 23:07:37 +0200

    run formatter

--- presyence/presyence/testrunner/discovery.py
+++ presyence/presyence/testrunner/discovery.py
@@ -8,7 +8,7 @@ import types
 
 
 def discover_test_files(pattern="**/*.spec.sy.py", directory=".") -> list[Path]:
-    """ This will discover file matching the specified pattern in the tree starting at the directory.
+    """This will discover file matching the specified pattern in the tree starting at the directory.
 
     Currently we enforce the pattern (ending in .spec.sy.py) and directory (pwd) but this is purely for simplicity.
     """
@@ -20,23 +20,24 @@ def discover_test_files(pattern="**/*.spec.sy.py", directory=".") -> list[Path]:
 
 
 def extract_tests_from_file(file):
-    """Will execute the .py file and get the value of the `TESTS` that should be inside.
-    """
+    """Will execute the .py file and get the value of the `TESTS` that should be inside."""
     # get the true absolute name of the file
     file = file.resolve()
     parent, name = file.parent, file.name
-    # add parent to path to help with relative imports 
+    # add parent to path to help with relative imports
     if str(parent) not in sys.path:
         sys.path.append(str(parent))
     # in the next lines we will import the file
     module_name = name
     spec = importlib.util.spec_from_file_location(module_name, file)
-    # test_module is the .py containing the tests to run 
+    # test_module is the .py containing the tests to run
     test_module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = test_module
     spec.loader.exec_module(test_module)
     # check that a lists named TESTS is present
     tests = test_module.__dict__.get("TESTS", [])
     if not tests:
-        raise NotImplementedError("Currently, all `.spec.sy.py` must contain a TESTS list containing SimpleTest")
+        raise NotImplementedError(
+            "Currently, all `.spec.sy.py` must contain a TESTS list containing SimpleTest"
+        )
     return tests
