3ee6b45ce5ed4086e3f52651aff897ef72462573
Rémi Connesson <remiconnesson@gmail.com>
2022-08-31 21:32:39 +0200

    Fix a typo and add comments to discovery.py

--- presyence/presyence/testrunner/discovery.py
+++ presyence/presyence/testrunner/discovery.py
@@ -7,7 +7,11 @@ import sys
 import types
 
 
-def discover_test_files(pattern="**/*.spec.sy.py", directory="."):
+def discover_test_files(pattern="**/*.spec.sy.py", directory=".") -> list[Path]:
+    """ This will discover file matching the specified pattern in the tree starting at the directory.
+
+    Currently we enforce the pattern (ending in .spec.sy.py) and directory (pwd) but this is purely for simplicity.
+    """
     p = Path(directory)
     test_files = p.glob(pattern)
     return list(
@@ -16,11 +20,13 @@ def discover_test_files(pattern="**/*.spec.sy.py", directory="."):
 
 
 def extract_tests_from_file(file):
+    """Will execute the .py file and get the value of the `TESTS` that should be inside.
+    """
     # get the true absolute name of the file
     file = file.resolve()
     parent, name = file.parent, file.name
     # add parent to path to help with relative imports 
-    if parent not in sys.path:
+    if str(parent) not in sys.path:
         sys.path.append(str(parent))
     # in the next lines we will import the file
     module_name = name
@@ -31,4 +37,6 @@ def extract_tests_from_file(file):
     spec.loader.exec_module(test_module)
     # check that a lists named TESTS is present
     tests = test_module.__dict__.get("TESTS", [])
+    if not tests:
+        raise NotImplementedError("Currently, all `.spec.sy.py` must contain a TESTS list containing SimpleTest")
     return tests
