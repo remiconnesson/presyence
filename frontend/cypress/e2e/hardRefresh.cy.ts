3b7bb069c6240d054505cb64d43c60143d50fd0b
Rémi Connesson <remiconnesson@gmail.com>
2022-08-31 17:27:46 +0200

    testing: renders HTML table after hard refresh

--- frontend/cypress/e2e/hardRefresh.cy.ts
+++ frontend/cypress/e2e/hardRefresh.cy.ts
@@ -7,11 +7,24 @@ describe("The Detailed Views survive an hard refresh", () => {
     ).contains("def");
   }
 
+  function rendersAnHTMLTable() {
+    /*
+     * Here we are checking that the table has 4 rows and 2 columns, which is possible, if and only if, the app has (1.) got the .csv from the pinia store, then (2.) parsed the CSV into an iterable and then (3.) built a HTML table from this iterable.
+     */
+    cy.get('[data-testid="test-detail-input"] > tbody ')
+      .children()
+      .should("have.length", 4);
+    cy.get('[data-testid="test-detail-input"] > tbody > :nth-child(3)')
+      .children()
+      .should("have.length", 2);
+  }
+
   function containsStuffTheyHaveInCommon() {
     cy.contains("h2", "The Expected Result");
     cy.contains("h2", "The Test Input");
     cy.contains("h2", "Tested Function");
     rendersHighlightedCode();
+    rendersAnHTMLTable();
   }
 
   it("hard refresh on a `WrongResult` and it renders properly", () => {
